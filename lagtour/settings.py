2b599b1287ae781c1ba225058d9b3e22501a759d
Oluwaseyi Adetuyi <seyispecial17@gmail.com>
2024-01-17 16:31:08 +0100

    Removed Debug and made allowed host to all

--- lagtour/settings.py
+++ lagtour/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jm(p50mx!dntnae*!!!g5k2%5io5ab895l#73&9$p=x7o017bi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
