d7dada1cf166052124e1989a663313828403f7b0
Oluwaseyi Adetuyi <seyispecial17@gmail.com>
2024-01-20 07:20:13 +0100

    Model changed

--- api/models.py
+++ api/models.py
@@ -5,12 +5,16 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True, max_length=2000)
+    opening_time = models.TimeField(null=True, blank=True)
+    closing_time = models.TimeField(null=True, blank=True)
+    location_area = models.CharField(null=True, blank=True, max_length=200)
     location_image = models.ImageField(
         null=True, blank=True, upload_to="images/")
     coordinate_x: models.IntegerField(null=True, blank=True)
     coordinate_y: models.IntegerField(null=True, blank=True)
     is_popular = models.BooleanField(default=False, blank=True, null=True)
-    wikipedia_link = models.URLField(default=False, blank=True, null=True)
+    more_info_link = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
