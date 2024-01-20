afb0262c226d8c5dd31be7d9be6ffb2b84e89403
Oluwaseyi Adetuyi <seyispecial17@gmail.com>
2024-01-20 05:52:57 +0100

    Added coordinates and wiki link

--- api/models.py
+++ api/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
-    location_image = models.ImageField(null=True,blank=True, upload_to="images/")
+    location_image = models.ImageField(
+        null=True, blank=True, upload_to="images/")
+    coordinate_x: models.IntegerField(null=True, blank=True)
+    coordinate_y: models.IntegerField(null=True, blank=True)
     is_popular = models.BooleanField(default=False, blank=True, null=True)
+    wikipedia_link = models.URLField(default=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
